428bf143ecd4b992863466f9059e4fa1de5f7800
KnoblauchPilze <prolific-programmer@tuta.com>
2025-05-24 12:04:38 +0200

    fix: Enhanced image plugin needs to be before svelte plugins

--- vite.config.ts
+++ vite.config.ts
@@ -4,7 +4,7 @@ import { sveltekit } from '@sveltejs/kit/vite';
 import tailwindcss from '@tailwindcss/vite';
 
 export default defineConfig({
-	plugins: [sveltekit(), tailwindcss(), enhancedImages()],
+	plugins: [enhancedImages(), sveltekit(), tailwindcss()],
 
 	test: {
 		include: ['src/**/*.{test,spec}.{js,ts}'],
