8f9a689e54d93de88b045e93ef266c32e228b78e
KnoblauchPilze <prolific-programmer@tuta.com>
2026-04-23 08:07:21 +0200

    fix: Lint

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -1,8 +1,8 @@
 <script lang="ts">
-  import "./layout.css";
-  import favicon from "$lib/assets/favicon.webp";
+	import './layout.css';
+	import favicon from '$lib/assets/favicon.webp';
 
-  let { children } = $props();
+	let { children } = $props();
 </script>
 
 <svelte:head><link rel="icon" href={favicon} /></svelte:head>
