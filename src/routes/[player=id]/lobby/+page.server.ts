fc9311b84a3d096d450f212434e3d7c5f7690362
Knoblauchpilze <prolific-programmer@tuta.com>
2026-08-18 08:08:25 +0200

    chore: Removed debug log

--- src/routes/[player=id]/lobby/+page.server.ts
+++ src/routes/[player=id]/lobby/+page.server.ts
@@ -10,7 +10,6 @@ export const load: PageServerLoad = async ({ params }) => {
 	const playerId = params.player;
 
 	const apiUserPlayers = await getPlayersByApiUser(playerId);
-	console.log(apiUserPlayers);
 
 	const players: LobbyPlayer[] = apiUserPlayers.map((player) => ({
 		universe: player.universe,
