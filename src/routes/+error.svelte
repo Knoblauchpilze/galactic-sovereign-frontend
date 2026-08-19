8a8c52aee11c46b019508af905172d28beff41ec
Knoblauchpilze <prolific-programmer@tuta.com>
2026-08-19 21:24:55 +0200

    feat: Added error page

---
+++ src/routes/+error.svelte
@@ -0,0 +1,8 @@
+<script lang="ts">
+	import { resolve } from '$app/paths';
+</script>
+
+<div class="flex flex-col items-center justify-center min-h-screen gap-4 bg-[#21211f] text-white">
+	<p class="text-lg font-medium">Oh noes, something went wrong!</p>
+	<a href={resolve('/')} class="text-sm text-blue-400 hover:underline">Return to login</a>
+</div>
